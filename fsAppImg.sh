3deb438318ade6ae7ae82d897dcd79f4385ff2d0
Jason Lethbridge <lethbridgejason@gmail.com>
2024-09-10 21:00:40 +1000

    Added Alchemy Viewer

--- fsAppImg.sh
+++ fsAppImg.sh
@@ -2,117 +2,139 @@
 
 # AI = AppImage
 AI_SURI="https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage"
-AI_CSUM="8897f478bb7b701fcd107503a08f62c4"
+AI_CSUM="md5 8897f478bb7b701fcd107503a08f62c4"
 AI_NAME="${AI_SURI##*/}"
 
-# FS = FireStorm
-FS_SURI="$2"
-FS_CSUM="$1"
-FS_NAME="${FS_SURI##*/}"
+# SL = SecondLife Viewer
+SL_SURI="$2"
+SL_CSUM="$1"
+SL_NAME="${SL_SURI##*/}"
 
 # VC = Voice Chat
 # 3p-slvoice from the FirestormViewer GitHub profile (commit d2977eb)
 # Used to supply dated but required 32-bit libraries for SLVoice (a.k.a Vivox) to run
 VC_SURI="https://github.com/FirestormViewer/3p-slvoice/archive/refs/heads/master.zip"
-VC_CSUM="274aa9261b9d4b360bcf2bc8c9760cb9"
+VC_CSUM="md5 274aa9261b9d4b360bcf2bc8c9760cb9"
 VC_NAME="3p-slvoice.zip"
 
 download_and_verify() {
 	local name="$1"
 	local suri="$2"
-	local csum="$3"
 
 	if [ ! -f "$name" ]; then
 		echo "Downloading $suri..."
 		wget -O "$name" "$suri"
 	fi
 
-	if [ ! -f "$name.md5" ]; then
-		echo "$csum  $name" > "$name.md5"
+	local array=()
+
+	# Split the hash type from the checksum
+	for element in $3; do
+		array+=("$element")
+	done
+
+	if [ ! -f "$name.${array[0]}" ]; then
+		echo "${array[1]}  $name" > "$name.${array[0]}"
 	fi
 
-	if ! md5sum -c "$name.md5"; then
-		echo "MD5 failed for $fs_name"
+	if ! "${array[0]}sum" -c "$name.${array[0]}"; then
+		echo "Checksum failed for $SL_name"
 		exit 1
 	fi
 }
 
 create_desktop_file() {
-	local path="$1"
-	cat << 'EOF' > "$path"
+	cat << EOF > "$1"
 [Desktop Entry]
-Name=Firestorm Viewer
+Name=${2^} Viewer
 Comment=Client for accessing 3D virtual worlds
-Exec=firestorm
-Icon=firestorm_icon
+Exec=${2}
+Icon=${2}_icon
 Terminal=false
 Type=Application
 Categories=Game;RolePlaying;Network;Chat;
 StartupNotify=true
 X-Desktop-File-Install-Version=3.0
-StartupWMClass=do-not-directly-run-firestorm-bin
+StartupWMClass=do-not-directly-run-${2}-bin
 EOF
 }
 
 create_apprun_script() {
-	local path="$1"
-	cat << 'EOF' > "$path"
+	cat << EOF > "$1"
 #!/bin/bash
-APPDIR="$(dirname "$(readlink -f "$0")")"
-exec "$APPDIR/firestorm" "$@"
+APPDIR="\$(dirname "\$(readlink -f "\$0")")"
+exec "\$APPDIR/$2" "\$@"
 EOF
 
-chmod +x "$path"
+	chmod +x "$1"
 }
 
 echo "Downloading and verifying content"
 
 download_and_verify "$AI_NAME" "$AI_SURI" "$AI_CSUM"
-download_and_verify "$FS_NAME" "$FS_SURI" "$FS_CSUM"
-download_and_verify "$VC_NAME" "$VC_SURI" "$VC_CSUM"
+download_and_verify "$SL_NAME" "$SL_SURI" "$SL_CSUM"
+
+if (($3 & 0x1)); then
+	download_and_verify "$VC_NAME" "$VC_SURI" "$VC_CSUM"
+fi
 
 # Download and verify has checked appimage against a good hash
 # Can now confidently be marked as an executable
 chmod +x "$AI_NAME"
 
-echo "Extracting $FS_NAME..."
+echo "Extracting $SL_NAME..."
 
 # Always work on a fresh extraction
-if [ -e AppDir ]; then
-	rm -R AppDir
+if [ -e "AppDir" ]; then
+	rm -R "AppDir"
 fi
 
 mkdir -p AppDir
-tar -xf "$FS_NAME" -C AppDir --strip-components=1
+tar -xf "$SL_NAME" -C AppDir --strip-components=1
 
+if (($3 & 0x1)); then
 echo "Extracting missing Vivox 32-bit libraries from $VC_NAME..."
 unzip -qnj "$VC_NAME" "3p-slvoice-master/bin/lib32/*" -d "AppDir/lib32"
+fi
 
 echo "Reconfiguring files in preperation for AppImage..."
 
 # Install scripts defeat the purposes of appimages
 # Could cause end-user confusion if the appimage is extracted
-rm "AppDir/install.sh" "AppDir/FIRESTORM_DESKTOPINSTALL.txt"
+if [ -e "AppDir/install.sh" ]; then rm "AppDir/install.sh"; fi
+if [ -e "AppDir/FIRESTORM_DESKTOPINSTALL.txt" ]; then rm "AppDir/FIRESTORM_DESKTOPINSTALL.txt"; fi
+
+# Get the viewer name we're working with
+if [ -f "AppDir/alchemy" ]; then
+	BIN="alchemy"
+elif [ -f "AppDir/firestorm" ]; then
+	BIN="firestorm"
+else
+	echo "Unknown viewer, can't continue"
+	exit 1
+fi
+
 
 # It's the normal way to start FireStorm but AppRun is what an AppImage will execute first
-create_apprun_script "AppDir/AppRun"
+create_apprun_script "AppDir/AppRun" "${BIN}"
 
 # Create a modified desktop entry to work within appimage
-create_desktop_file "AppDir/firestorm.desktop"
+create_desktop_file "AppDir/${BIN}.desktop" "${BIN}"
 
-OUTPUT="${FS_NAME%%.*}-x86_64.AppImage"
+OUTPUT="${SL_NAME%%.*}-x86_64.AppImage"
 
 # Remove Releasex64 from the AppImage name.
 # Appimage standardize putting the ISA on the end of the file name
 OUTPUT="${OUTPUT//-Releasex64}"
+OUTPUT="${OUTPUT//_x86_64}"
 
 echo "Creating AppImage $OUTPUT..."
 
 ARCH=x86_64 "./$AI_NAME" -n AppDir "$OUTPUT"
 
-echo "Zipping $OUTPUT into ${OUTPUT%%.*}.zip..."
+echo "Zipping $OUTPUT into ${OUTPUT}.zip..."
 
-zip -0 "${OUTPUT%%.*}.zip" "$OUTPUT"
+zip -0 "${OUTPUT}.zip" "$OUTPUT"
 
 echo "Cleaning up..."
 
--- manifest.sh
+++ manifest.sh
@@ -2,32 +2,40 @@
 
 declare -A manifest
 
-# Manifest array [x, 0] = MD5 checksum
+# Manifest array [x, 0] = Checksum
 # Manifest array [x, 1] = Tarball URL
+# Manifest array [x, 2] = Bitflags, 0x1 = Vivox
 
-manifest[0,0]="46ca48b9db04d7aba8c03b98a00fbd83"
+manifest[0,0]="md5 46ca48b9db04d7aba8c03b98a00fbd83"
 manifest[0,1]="https://downloads.firestormviewer.org/release/linux/Phoenix-Firestorm-Releasex64-6-6-14-69596.tar.xz"
+manifest[0,2]=0x1
 
-manifest[1,0]="9b0ab83c0ae1365b58fd77307dfba769"
+manifest[1,0]="md5 9b0ab83c0ae1365b58fd77307dfba769"
 manifest[1,1]="https://downloads.firestormviewer.org/release/linux/Phoenix-Firestorm-Releasex64-6-6-17-70368.tar.xz"
+manifest[1,2]=0x1
 
-manifest[2,0]="c5638069ec5ad9dee3f4e44e7e18116e"
+manifest[2,0]="md5 c5638069ec5ad9dee3f4e44e7e18116e"
 manifest[2,1]="https://downloads.firestormviewer.org/release/linux/Phoenix-Firestorm-Releasex64-7-1-9-74745.tar.xz"
+manifest[2,2]=0x1
 
-MAX=3 # Total number of manifest entries
+manifest[3,0]="sha256 370fa700c4cf1ccf9e16a933d5bb056fc3adb783971aba4c238797ff0324301e"
+manifest[3,1]="https://github.com/AlchemyViewer/Alchemy/releases/download/7.1.9.2501-beta/Alchemy_Beta_7_1_9_2501_x86_64.tar.xz"
+manifest[3,2]=0x0 # This viewer uses WebRTC for voice chat
+
+MAX=4 # Total number of manifest entries
 
 # Print the options
 for ((i = 0; i < MAX; i++)); do
-    url="${manifest[$i,1]}"
-    filename="${url##*/}"
-    filename="${filename%%.*}"
-    echo "$i: $filename"
+	url="${manifest[$i,1]}"
+	filename="${url##*/}"
+	filename="${filename%%.*}"
+	echo "$i: $filename"
 done
 
 read -p "Select a option by number: " row
 
 if [[ $row -ge 0 && $row -lt $MAX ]]; then
-	./fsAppImg.sh "${manifest[$row,0]}" "${manifest[$row,1]}"
+	./fsAppImg.sh "${manifest[$row,0]}" "${manifest[$row,1]}" "${manifest[$row,2]}"
 else
 	echo "Invalid selection."
 fi
