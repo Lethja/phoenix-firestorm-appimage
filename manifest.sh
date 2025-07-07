977fdd200f6edb4f1b01a063c19f3ac2be2ec120
Jason Lethbridge <lethbridgejason@gmail.com>
2025-07-08 00:20:10 +1000

    Added Firestorm 7.1.13-78266

--- manifest.sh
+++ manifest.sh
@@ -42,7 +42,11 @@ manifest[8,0]="sha256 163a8353b1ec91f42466580b853fe031d97d60746768d970bf105461d2
 manifest[8,1]="https://github.com/AlchemyViewer/Alchemy/releases/download/7.1.9.2516-beta/Alchemy_Beta_7_1_9_2516_x86_64.tar.zst"
 manifest[8,2]=0x0 # This viewer uses WebRTC for voice chat
 
-MAX=9 # Total number of manifest entries
+manifest[9,0]="md5 f1f8903b5ca4f4a841fae465cfbf3466"
+manifest[9,1]="https://downloads.firestormviewer.org/release/linux/Phoenix-Firestorm-Releasex64_AVX2-7-1-13-78266.tar.xz"
+manifest[9,2]=0x0 # This viewer uses WebRTC for voice chat
+
+MAX=10 # Total number of manifest entries
 
 print_options() {
 	for ((i = 0; i < MAX; i++)); do
